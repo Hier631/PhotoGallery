595824e9cf9c864fffeaa2b4af8dd7c6325dee81
RedSpark277 <redspark277@outlook.com>
2022-08-31 07:08:31 +0200

    Add TitleFragment class.

---
+++ app/src/main/java/com/outlook/redspark277/photogallery/fragments/TitleFragment.java
@@ -0,0 +1,42 @@
+package com.outlook.redspark277.photogallery.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.outlook.redspark277.photogallery.R;
+import com.outlook.redspark277.photogallery.databinding.FragmentTitleBinding;
+
+public class TitleFragment extends Fragment {
+
+    FragmentTitleBinding binding;
+    NavController naVController;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_title, container, false);
+        naVController = Navigation.findNavController(requireActivity(), R.id.frgNavHostFragment);
+
+        binding.btnLoadGallery.setOnClickListener(v -> {
+
+        });
+
+        return binding.getRoot();
+    }
+}
